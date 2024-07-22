f0ca71f928460c49796fd86d92331b226b381302
EricReno <15192543063@163.com>
2024-07-22 17:24:40 +0800

    fix:export_onnx

--- deploy/export_onnx.py
+++ deploy/export_onnx.py
@@ -6,38 +6,47 @@ sys.path.append('../')
 from config import parse_args
 from model.yolov1 import YOLOv1
 
-args = parse_args()
+def export(input, model, pt_path, onnx_name):
+    model.eval()
+    model.deploy = True
 
-x = torch.randn(1, 3, 640, 640)
+    ckpt = os.getcwd().replace('deploy', pt_path)
+    state_dict = torch.load(ckpt, map_location='cpu')
+    model.load_state_dict(state_dict["model"])
 
-model = YOLOv1(args = args, 
+    with torch.no_grad():
+        torch.onnx.export(
+            model,
+            input,
+            "yolo_v1.onnx",
+            opset_version=11,
+            input_names=['input'],
+            output_names=['output'])
+
+    # 添加中间层特征尺寸
+    onnx_model = onnx.load(onnx_name) 
+    onnx.save(onnx.shape_inference.infer_shapes(onnx_model), onnx_name)
+
+    try: 
+        onnx.checker.check_model(onnx_model) 
+    except Exception: 
+        print("Model incorrect")
+    else: 
+        print("Model correct")
+
+if __name__ == "__main__":
+    version = "yolo_v1"
+    pt_path = "results/84.pth"
+
+    args = parse_args()
+
+    x = torch.randn(1, 3, 640, 640)
+
+    if version == "yolo_v1":
+        model = YOLOv1(args = args, 
                device = torch.device('cpu'),
                trainable = False,
                nms_thresh = args.nms_thresh,
                conf_thresh = args.conf_thresh)
-model.deploy = True
 
-ckpt_path = os.path.join(args.root, args.project, 'results', '82.pth')
-ckpt_state_dict = torch.load(ckpt_path, map_location='cpu')["model"]
-model.load_state_dict(ckpt_state_dict)
-model = model.eval()
-
-
-with torch.no_grad():
-    torch.onnx.export(
-        model,
-        x,
-        "yolo_v1.onnx",
-        opset_version=11,
-        input_names=['input'],
-        output_names=['output'])
-
-onnx_model = onnx.load("yolo_v1.onnx") 
-onnx.save(onnx.shape_inference.infer_shapes(onnx_model), "yolo_v1.onnx")
-
-try: 
-    onnx.checker.check_model(onnx_model) 
-except Exception: 
-    print("Model incorrect") 
-else: 
-    print("Model correct")
+    export(x, model, pt_path, version+".onnx")
